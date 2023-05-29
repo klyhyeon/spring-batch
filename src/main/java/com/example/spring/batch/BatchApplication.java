be6a1fa2d03c4b980b74b1bead6c19b1f2d896e5
baekho <klyhyeon@gmail.com>
2023-05-29 15:30:10 +0900

    feat: 예제 4-8 완료

--- src/main/java/com/example/spring/batch/BatchApplication.java
+++ src/main/java/com/example/spring/batch/BatchApplication.java
@@ -2,10 +2,10 @@ package com.example.spring.batch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,10 +18,8 @@ public class BatchApplication {
     //jobRepository, platfromTrasactionManager는 SpringBatchConfiguration에서 자동으로 주입
     @Bean
     public Step step1(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
-        return new StepBuilder("step1", jobRepository).tasklet((contribution, chunkContext) -> {
-            System.out.println("Hello, World!");
-            return RepeatStatus.FINISHED;
-        }, platformTransactionManager).build();
+        return new StepBuilder("step1", jobRepository)
+                .tasklet(helloWorldTasklet(), platformTransactionManager).build();
     }
 
     @Bean
@@ -29,6 +27,17 @@ public class BatchApplication {
         return new JobBuilder("basicJob", jobRepository)
                 .start(step1(jobRepository, platformTransactionManager))
                 .build();
+    }
+
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return ((contribution, chunkContext) -> {
+            String name = (String) chunkContext.getStepContext()
+                    .getJobParameters()
+                    .get("name");
+            System.out.println(String.format("Hello, %s!", name));
+            return RepeatStatus.FINISHED;
+        });
     }
 
 
